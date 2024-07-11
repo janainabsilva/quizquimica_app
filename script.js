317b09f8767479142e36c73c340d9ae624df7664
janabsilva <janainabatistacpro@gmail.com>
2024-07-10 23:30:29 -0300

    adicionada a função para escolher o tema do quiz no index.html

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 import { trocarTema,verificarTema} from "./helpers/tema-helper.js"
 
 
-
-
 const botaoTema = document.querySelector(".tema button")
 
 const body = document.querySelector("body")
@@ -14,5 +12,14 @@ botaoTema.addEventListener("click", () => {
 
 verificarTema(body,botaoTema)
 
+const botoesAssunto = document.querySelectorAll(".assuntos button")
+botoesAssunto.forEach(botao => {
+    botao.addEventListener("click", selecionarAssunto)
+})
 
+function selecionarAssunto(evento){
+    const assunto = evento.target.innerText
+    localStorage.setItem("assunto" , assunto)
+    window.location.href = "./paginas/quiz/quiz.html"
+}
 
